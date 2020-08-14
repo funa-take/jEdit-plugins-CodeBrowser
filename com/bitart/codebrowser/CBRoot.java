b31c2e63f43738e85c873a5d41899e69ff27f4eb
funatake <centos@localhost.localdomain>
2020-08-14 19:35:35 +0900

    UTF-8Yでも動作するように修正

--- com/bitart/codebrowser/CBRoot.java
+++ com/bitart/codebrowser/CBRoot.java
@@ -106,6 +106,9 @@ public class CBRoot implements TreeNode
       
       if (upperEncoding.indexOf("NATIVE2ASCII") == 0){
         encoding = "ISO-8859-1";
+      } else if (upperEncoding.indexOf("UTF-8") >= 0){
+        // UTF-8YもUTF-8として処理する
+        encoding = "UTF-8";
       }
       
       // System.out.println(encoding);
@@ -114,34 +117,34 @@ public class CBRoot implements TreeNode
       
       String ctagsLang = getCtagsLang(lang, fileName);
       
-			String[] args;
-			
-			if(!buildxml)
-			{
-				args=new String[]{
-					jEdit.getProperty("options.codebrowser.ctags_path"),
-					"--fields=KsSz",
-					"--excmd=pattern",
-					"--sort=no",
-					"-f",
-					"-",
-					path
-				};
-			}
-			else
-			{
-				args=new String[]{
-					jEdit.getProperty("options.codebrowser.ctags_path"),
-					"--fields=KsSz",
-					"--excmd=pattern",
-					"--sort=no",
-					"--language-force=ant",
-					"-f",
-					"-",
-					path
-				};
-				lang="ant";
-			}
+      String[] args;
+      
+      if(!buildxml)
+      {
+        args=new String[]{
+          jEdit.getProperty("options.codebrowser.ctags_path"),
+          "--fields=KsSz",
+          "--excmd=pattern",
+          "--sort=no",
+          "-f",
+          "-",
+          path
+        };
+      }
+      else
+      {
+        args=new String[]{
+          jEdit.getProperty("options.codebrowser.ctags_path"),
+          "--fields=KsSz",
+          "--excmd=pattern",
+          "--sort=no",
+          "--language-force=ant",
+          "-f",
+          "-",
+          path
+        };
+        lang="ant";
+      }
       // funa edit
       if (!ctagsEncoding.equals("")){
         String[] newArgs = new String[args.length + 1];
@@ -159,215 +162,215 @@ public class CBRoot implements TreeNode
         args = newArgs;
       }
       
-			/*
-			System.err.println("Args: ");
-			for(int i=0;i<args.length;i++)
-			{
-			System.err.println("\t"+args[i]);
-			}
-			*/
-			Process p=Runtime.getRuntime().exec(args);
-			BufferedReader in=new BufferedReader(new InputStreamReader(p.getInputStream(), encoding));
-			//System.err.println("ctags started!");
-			
-			String line;
-			while((line=in.readLine())!=null)
-			{
-				//System.err.println("Got line: "+line);
-				// Get rid of crlf
-				while(line.endsWith("\n") || line.endsWith("\r"))
-				{
-					line=line.substring(0,line.length()-1);
-				}
-				
-				//
-				// split off extension
-				//
-				int idx;
-				idx=line.lastIndexOf(";\"\t");
-				if(idx<0) continue;
-				
-				// extensions in Vector v, remove from line
-				Vector v=split("\t",line.substring(idx+3));
-				line=line.substring(0,idx);
-				
-				// Create a hash from extensions
-				Hashtable info=new Hashtable();
-				for(int i=0;i<v.size();i++)
-				{
-					String s=(String)v.elementAt(i);
-					int ei=s.indexOf(':');
-					if(ei<0) continue;
-					info.put(s.substring(0,ei),s.substring(ei+1));
-				}
-				
-				// item name
-				idx=line.indexOf("\t");
-				if(idx<0) continue;
-				info.put("cb_tag_cb",line.substring(0,idx));
-				line=line.substring(idx+1);
-				
-				// file name, not needed
-				idx=line.indexOf("\t");
-				if(idx<0) continue;
-				//info.put("cb_file_cb",line.substring(0,idx));
-				
-				// pattern
-				info.put("cb_pattern_cb",line.substring(idx+1));
-				
-				//System.err.println("Parsed into: "+info);
-				
-				String type=(String)info.get("kind");
-				if(type==null || type.length()==0) continue;
-				
-				CBType t=(CBType)cbTypes.get(type);
-				if(t==null)
-				{
-					t=new CBType(this,type,lang);
-					cbTypes.put(type,t);
-					tv.add(type);
-				}
-				t.add(info);
-			}
-		}
-		catch(IOException ioe)
-		{
-			System.err.println(ioe);
-		}
-		//System.err.println("Done reading");
-		
-		
-		Collections.sort(tv);
-		for(int i=0;i<tv.size();i++)
-		{
-			children.add(cbTypes.get(tv.elementAt(i)));
-		}
-	}
-	
-	private String getCtagsLang(String mode, String fileName) {
-	  Vector<Vector<String>> mapping = CodeBrowserOptionPane.getMapping();
-	  
-	  for(int i = 0; i < mapping.size(); i++) {
-	    String mappingMode = mapping.get(i).get(CodeBrowserOptionPane.MappingModel.COL.MODE.ordinal());
-	    String mappingRegex = mapping.get(i).get(CodeBrowserOptionPane.MappingModel.COL.FILE_REGEX.ordinal());
-	    String mappingLang = mapping.get(i).get(CodeBrowserOptionPane.MappingModel.COL.LANG.ordinal());
-	    
-	    if (!"".equals(mappingMode) && !mappingMode.toLowerCase().equals(mode.toLowerCase())) {
-	      continue;
-	    }
-	    
-	    if (!"".equals(mappingRegex) && !Pattern.matches(mappingRegex, fileName)) {
-	      continue;
-	    }
-	    
-	    return mappingLang;
-	  }
-	  
-	  return "";
-	}
-	
-	public Vector split(String where,String str)
-	/***********************************************************************
-	 * Splits the String txt on occurances of str, returns a Vector
-	 * of Strings.
-	 * @param where The String to split on.
-	 * @param str The String to split.
-	 * @return A Vector of strings.
-	 ***********************************************************************/
-	{
- 		Vector v=new Vector();
-		
-		int idx;
-		
-		while((idx=str.indexOf(where))>=0)
-		{
-			String s="";
-			if(idx>0) s=str.substring(0,idx);
-			v.addElement(s);
-			str=str.substring(idx+where.length());
-		}
-		v.addElement(str);
-		
-		return v;
-	}
-	
-	public void expandPaths(JTree tree)
-	{
-		Object[] objs={
-			this,
-			this
-		};
-		for(int i=children.size()-1;i>=0;i--)
-		{
-			CBType t=(CBType)children.elementAt(i);
-			if(t.getState())
-			{
-				objs[1]=t;
-				tree.expandPath(new TreePath(objs));
-			}
-		}
-	}
-	
-	public void setSorted(boolean flag,JTree tree)
-	{
-		DefaultTreeModel tm=null;
-		if(tree!=null) tm=(DefaultTreeModel)tree.getModel();
-		
-		for(int i=0;i<children.size();i++)
-		{
-			CBType t=(CBType)children.elementAt(i);
-			t.setSorted(flag);
-			if(tm!=null && t.getState())
-			{
-				int l=t.getChildCount();
-				int[] idc=new int[l];
-				for(int j=0;j<l;j++)
-				{
-					idc[j]=j;
-				}
-				
-				tm.nodesChanged(t,idc);
-			}
-		}
-	}
-	
-	/******************************************************************************
-	 * TreeNode interface
-	 ******************************************************************************/
-	public Enumeration children()
-	{
-		return children.elements();
-	}
-	
-	public boolean getAllowsChildren()
-	{
-		return true;
-	}
-	
-	public TreeNode getChildAt(int index)
-	{
-		return (TreeNode)children.elementAt(index);
-	}
-	
-	public int getChildCount()
-	{
-		return children.size();
-	}
-	
-	public int getIndex(TreeNode child)
-	{
-		return children.indexOf(child);
-	}
-	
-	public TreeNode getParent()
-	{
-		return null;
-	}
-	
-	public boolean isLeaf()
-	{
-		return false;
-	}
+      /*
+      System.err.println("Args: ");
+      for(int i=0;i<args.length;i++)
+      {
+      System.err.println("\t"+args[i]);
+      }
+      */
+      Process p=Runtime.getRuntime().exec(args);
+      BufferedReader in=new BufferedReader(new InputStreamReader(p.getInputStream(), encoding));
+      //System.err.println("ctags started!");
+      
+      String line;
+      while((line=in.readLine())!=null)
+      {
+        //System.err.println("Got line: "+line);
+        // Get rid of crlf
+        while(line.endsWith("\n") || line.endsWith("\r"))
+        {
+          line=line.substring(0,line.length()-1);
+        }
+        
+        //
+        // split off extension
+        //
+        int idx;
+        idx=line.lastIndexOf(";\"\t");
+        if(idx<0) continue;
+        
+        // extensions in Vector v, remove from line
+        Vector v=split("\t",line.substring(idx+3));
+        line=line.substring(0,idx);
+        
+        // Create a hash from extensions
+        Hashtable info=new Hashtable();
+        for(int i=0;i<v.size();i++)
+        {
+          String s=(String)v.elementAt(i);
+          int ei=s.indexOf(':');
+          if(ei<0) continue;
+          info.put(s.substring(0,ei),s.substring(ei+1));
+        }
+        
+        // item name
+        idx=line.indexOf("\t");
+        if(idx<0) continue;
+        info.put("cb_tag_cb",line.substring(0,idx));
+        line=line.substring(idx+1);
+        
+        // file name, not needed
+        idx=line.indexOf("\t");
+        if(idx<0) continue;
+        //info.put("cb_file_cb",line.substring(0,idx));
+        
+        // pattern
+        info.put("cb_pattern_cb",line.substring(idx+1));
+        
+        //System.err.println("Parsed into: "+info);
+        
+        String type=(String)info.get("kind");
+        if(type==null || type.length()==0) continue;
+        
+        CBType t=(CBType)cbTypes.get(type);
+        if(t==null)
+        {
+          t=new CBType(this,type,lang);
+          cbTypes.put(type,t);
+          tv.add(type);
+        }
+        t.add(info);
+      }
+    }
+    catch(IOException ioe)
+    {
+      System.err.println(ioe);
+    }
+    //System.err.println("Done reading");
+    
+    
+    Collections.sort(tv);
+    for(int i=0;i<tv.size();i++)
+    {
+      children.add(cbTypes.get(tv.elementAt(i)));
+    }
+  }
+  
+  private String getCtagsLang(String mode, String fileName) {
+    Vector<Vector<String>> mapping = CodeBrowserOptionPane.getMapping();
+    
+    for(int i = 0; i < mapping.size(); i++) {
+      String mappingMode = mapping.get(i).get(CodeBrowserOptionPane.MappingModel.COL.MODE.ordinal());
+      String mappingRegex = mapping.get(i).get(CodeBrowserOptionPane.MappingModel.COL.FILE_REGEX.ordinal());
+      String mappingLang = mapping.get(i).get(CodeBrowserOptionPane.MappingModel.COL.LANG.ordinal());
+      
+      if (!"".equals(mappingMode) && !mappingMode.toLowerCase().equals(mode.toLowerCase())) {
+        continue;
+      }
+      
+      if (!"".equals(mappingRegex) && !Pattern.matches(mappingRegex, fileName)) {
+        continue;
+      }
+      
+      return mappingLang;
+    }
+    
+    return "";
+  }
+  
+  public Vector split(String where,String str)
+  /***********************************************************************
+   * Splits the String txt on occurances of str, returns a Vector
+   * of Strings.
+   * @param where The String to split on.
+   * @param str The String to split.
+   * @return A Vector of strings.
+   ***********************************************************************/
+  {
+    Vector v=new Vector();
+    
+    int idx;
+    
+    while((idx=str.indexOf(where))>=0)
+    {
+      String s="";
+      if(idx>0) s=str.substring(0,idx);
+      v.addElement(s);
+      str=str.substring(idx+where.length());
+    }
+    v.addElement(str);
+    
+    return v;
+  }
+  
+  public void expandPaths(JTree tree)
+  {
+    Object[] objs={
+      this,
+      this
+    };
+    for(int i=children.size()-1;i>=0;i--)
+    {
+      CBType t=(CBType)children.elementAt(i);
+      if(t.getState())
+      {
+        objs[1]=t;
+        tree.expandPath(new TreePath(objs));
+      }
+    }
+  }
+  
+  public void setSorted(boolean flag,JTree tree)
+  {
+    DefaultTreeModel tm=null;
+    if(tree!=null) tm=(DefaultTreeModel)tree.getModel();
+    
+    for(int i=0;i<children.size();i++)
+    {
+      CBType t=(CBType)children.elementAt(i);
+      t.setSorted(flag);
+      if(tm!=null && t.getState())
+      {
+        int l=t.getChildCount();
+        int[] idc=new int[l];
+        for(int j=0;j<l;j++)
+        {
+          idc[j]=j;
+        }
+        
+        tm.nodesChanged(t,idc);
+      }
+    }
+  }
+  
+  /******************************************************************************
+   * TreeNode interface
+   ******************************************************************************/
+  public Enumeration children()
+  {
+    return children.elements();
+  }
+  
+  public boolean getAllowsChildren()
+  {
+    return true;
+  }
+  
+  public TreeNode getChildAt(int index)
+  {
+    return (TreeNode)children.elementAt(index);
+  }
+  
+  public int getChildCount()
+  {
+    return children.size();
+  }
+  
+  public int getIndex(TreeNode child)
+  {
+    return children.indexOf(child);
+  }
+  
+  public TreeNode getParent()
+  {
+    return null;
+  }
+  
+  public boolean isLeaf()
+  {
+    return false;
+  }
 }
 /*************************************************************************EOF*/
 
